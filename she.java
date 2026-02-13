acbd944fe5ad8c1d99e379081087b806550fd3a7
lekhandhruw160-max <lekhandhruw160@gmail.com>
2026-02-13 18:34:36 +0530

    add java

---
+++ she.java
@@ -0,0 +1,12 @@
+
+public class she {
+    public static void main(String[] args) {
+        System.out.print("lekhan");
+        int num = 34;
+        String name = "shankar ";
+
+        System.out.println(num);
+        System.out.println(name);
+    }
+    
+}
