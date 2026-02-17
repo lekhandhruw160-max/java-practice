8e6338294b8de7d85ee73c6a95ae6ebd9c766294
lekhandhruw160-max <lekhandhruw160@gmail.com>
2026-02-17 20:31:50 +0530

    add

--- she.java
+++ she.java
@@ -1,42 +1,20 @@
-import java.util.*;
-public class she{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = sc.nextInt();
-        
-        switch (sum){
-            case 1: System.out.println(a+b);
-            break;
-            case 2: System.out.println(a-b);
-            break;
-            case 3: System.out.println(a*b);
-            break;
-            case 4:
-                if (b == 0){
-                    System.out.println ("Invalid number");
-                }
-                else {
-                    System.out.println (a%b);
-                }
-            break;
-            case 5: 
-                if (b == 0){
-                    System.out.println ("Invalid nubmer");
-                }
-                else{
-                    System.out.println ( a/b);
-                }    
-            break;
-    
+public class she{ 
+    public static void main(String  args[]){
+        int n = 5;
+
+        for ( int i = 1; i<= n; i++){
+            for (int j = 1; j<=n; j++){
+                System. out.print("*");
+
+            }
+            System.out.println(" ");
         }
+
+
         
-
-
-
-
-
-
+   
+        
+        
     }
+
 }
