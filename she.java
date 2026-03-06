586eb8de63f6b54bb3d6512da4d746bcaf50da4a
lekhandhruw160-max <lekhandhruw160@gmail.com>
2026-03-06 12:23:35 +0530

    add

--- she.java
+++ she.java
@@ -1,17 +1,16 @@
+import java.util.*;
+
 public class she{
-    public static void main(String  args[]){
-        int n = 5;
-        int m = 4;
-        for (int i = 1; i <= n; i++){
-            for(int j = 1; j<=m; j++){
-                if (i == 1 || j == 1 || i == n || j == m){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number :");
+        int n = sc.nextInt();
+        for(int  i = 1; i<= n; i++ ){
+            for( int j = 1; j<=n; j++){
+                System.out.print("*"+" ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
+
     }
 }
